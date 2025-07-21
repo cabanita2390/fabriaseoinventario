6506b4574ad661cb76d42816490126a3d5da64ca
Felipe Cabana <cabanita2390@hotmail.com>
2025-07-21 16:52:36 -0500

    Se activa nuevamente guards de gestion de usuairos

--- backend/src/usuario/usuario.controller.ts
+++ backend/src/usuario/usuario.controller.ts
@@ -17,7 +17,7 @@ import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { ADMIN } from 'src/auth/constants/roles.constant';
 
-// @UseGuards(JwtAuthGuard, RolesGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('usuario')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
